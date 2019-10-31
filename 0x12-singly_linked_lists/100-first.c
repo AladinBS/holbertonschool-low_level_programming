7adc55f6b18f18bf302a18fbba5aaf16709ce52e
AladinBS <aladin.bensassi@gmail.com>
2019-10-31 16:26:27 +0000

    a big push

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* bmain - function executed before the main G
+* Return: no, there will be no returns, ever, again // Aladin Bensassi
+* PS: I hate bonus tasks.
+*/
+
+void __attribute__ ((constructor)) bmain()
+{
+printf("You're beat! and yet, you must allow");
+printf(",\nI bore my house upon my back!\n");
+}
