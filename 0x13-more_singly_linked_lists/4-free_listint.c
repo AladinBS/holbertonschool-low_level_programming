a028f29e79f32446d1900f435373efd04d3a87ed
AladinBS <aladin.bensassi@gmail.com>
2019-11-05 18:01:09 +0000

    Fix

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+* free_listint - frees a linked list
+* @head: head of a list.
+* Return: no return. //Aladin Bensassi
+*/
+void free_listint(listint_t *head)
+{
+listint_t *temp;
+while ((temp = head) != NULL)
+{
+head = head->next;
+free(temp);
+}
+}
