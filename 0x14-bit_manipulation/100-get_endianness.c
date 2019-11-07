61e38db93ebe15beea2feb2fe85c9c25fb3487d0
AladinBS <aladin.bensassi@gmail.com>
2019-11-07 20:54:17 +0000

    add

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* get_endianness - checks the endianness
+* Return: 0 if big endian, 1 if little endian
+* Aladin Bensassi
+*/
+int get_endianness(void)
+{
+unsigned int x;
+char *c;
+x = 1;
+c = (char *) &x;
+return ((int)*c);
+}
