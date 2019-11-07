d4c3915dc06b89294a2219b94e30e2e01df7f890
AladinBS <aladin.bensassi@gmail.com>
2019-11-07 21:16:02 +0000

    fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,21 +1,27 @@
 #include "holberton.h"
 /**
-* binary_to_uint - converts a binary number to an unsigned int
-* @b: pointer to a string of 0 and 1 chars
-* Return: converted to base 10. //Aladin BS
+* binary_to_uint - converts a binary nbr to an int.
+* @b: pointer to a string containing a binary nbr
+* Return: unsigned int //Aladin Bensassi
 */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int number = 0;
-if (b == NULL)
+int x;
+unsigned int cal = 0;
+if (!b)
 return (0);
-while (*b)
+x = 0;
+while (b[x] != '\0')
 {
-if (*b != '1' && *b != '0')
+if (b[x] != '0' && b[x] != '1')
 return (0);
-number *= 2;
-number += (*b == '1');
-b++;
+x++;
 }
-return (number);
+for (x = 0; b[x] != '\0'; x++)
+{
+cal <<= 1;
+if (b[x] == '1')
+cal += 1;
+}
+return (cal);
 }
