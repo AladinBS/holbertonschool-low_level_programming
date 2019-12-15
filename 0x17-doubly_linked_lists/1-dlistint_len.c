221d4d5c8f302ec648d7696013bb93889157fb4b
AladinBS <aladin.bensassi@gmail.com>
2019-12-15 21:53:29 +0000

    big push

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+* dlistint_len - returns the number of elements in
+* a double linked list
+* @h: head of the list
+* Return: the number of nodes
+* Aladin Bensassi
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+int x;
+x = 0;
+if (h == NULL)
+return (x);
+while (h->prev != NULL)
+h = h->prev;
+while (h != NULL)
+{
+x++;
+h = h->next;
+}
+return (x);
+}
