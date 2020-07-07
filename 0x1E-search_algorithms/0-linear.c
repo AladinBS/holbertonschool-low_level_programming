e7f3deafb751bb38f638fc200940b2310a58bd9b
AladinBS <aladin.bensassi@gmail.com>
2020-07-07 13:34:07 +0100

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,26 +1,25 @@
 #include "search_algos.h"
 
 /**
-* linear_search - Function
-* @value: value
-* Return: The index
-* @array: array
-* @size: size
-*/
+ * linear_search - Function
+ */
 int linear_search(int *array, size_t size, int value)
 {
-int position = 0;
-if (array == NULL)
-return (-1);
-/*
-* Size
-*/
-while (position < (int)size)
-{
-printf("Value checked array[%d] = [%d]\n", position, array[position]);
-if (array[position] == value)
-return (position);
-position++;
-}
-return (-1);
+	int position = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	/*
+	 * Size
+	 */
+	while (position < (int)size)
+	{
+		printf("Value checked array[%d] = [%d]\n", position, array[position]);
+		if (array[position] == value)
+			return (position);
+
+		position++;
+	}
+	return (-1);
 }
